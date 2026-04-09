3a4a4f5559f7c43942595449d4a401086cd20edd
Ryan <rcadwell91@gmail.com>
2026-04-09 13:15:54 -0600

    feat: add recursive static file copying and automated build pipeline

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+from copystatic import copy_files_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+
+def main():
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+main()
+
+if __name__ == "__main__":
+    main()
