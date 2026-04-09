1b41b2b4a8d49e6280324362fe0d8b763042b472
Ryan <rcadwell91@gmail.com>
2026-04-09 13:19:48 -0600

    test: add unit test suites and CI shell scripts

---
+++ src/test_htmlnode.py
@@ -0,0 +1,62 @@
+import unittest
+from htmlnode import HTMLNode, LeafNode, ParentNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        node = HTMLNode(
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "id": "main"},
+        )
+        self.assertEqual(
+            node.props_to_html(),
+            ' class="greeting" id="main"',
+        )
+
+    def test_values(self):
+        node = HTMLNode(
+            "p",
+            "This is a paragraph",
+        )
+        self.assertEqual(node.tag, "p")
+        self.assertEqual(node.value, "This is a paragraph")
+        self.assertEqual(node.children, None)
+        self.assertEqual(node.props, None)
+
+    def test_repr(self):
+        node = HTMLNode(
+            "p",
+            "What a strange world",
+            None,
+            {"class": "primary"},
+        )
+        self.assertEqual(
+            node.__repr__(),
+            "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
+        )
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_raw(self):
+        node = LeafNode(None, "Just some text")
+        self.assertEqual(node.to_html(), "Just some text")
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+    )
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/test_textnode.py
@@ -0,0 +1,48 @@
+import unittest
+from textnode import TextNode, TextType, text_node_to_html_node
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_eq_false(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a different text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, bold, https://www.boot.dev)", repr(node)
+        )
+
+    def test_eq_url_none(self):
+        node = TextNode("This is a text node", TextType.BOLD, None)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.google.com", "alt": "This is an image"},
+        )
+
+if __name__ == "__main__":
+    unittest.main()
